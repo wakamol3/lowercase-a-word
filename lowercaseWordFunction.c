84a6fe996b84d028aa0275399afb31e5182efe70
wakamol3 <74549131+wakamol3@users.noreply.github.com>
2023-04-12 18:57:03 -0400

    Update lowercaseWordFunction.c

--- lowercaseWordFunction.c
+++ lowercaseWordFunction.c
@@ -4,31 +4,37 @@
 #include<ctype.h>
 #include<string.h>
 
+//declare the function prototype - takes 2 char arrays for the word and the lowercase word
 void lowerIt(char word[], char lowercase[]);
 
 int main()
 {
+	//assign char array for word and the lowercase word
 	char word[25];
 	char lowercase[25];
 
+	//get the word and store it in the word array
 	printf("Print a word ");
 	scanf(" %s", word);
 
+	//call the function to make it lowercase
 	lowerIt(word, lowercase);
 
-	printf("\nPrinting the word in main\n");
-
+	//printed word and lowercase word in main
 	printf("\nYour word is %s, and lowercase is %s", word, lowercase);
 
 	return 0;
 }
-
+//function definition
 void lowerIt(char word[], char lowercase[])
 {
+	//declare int variables for the for loop, we need the length of the string for the loop and we need i for an iterator.
 	int i, len = strlen(word);
 	for (i = 0; i < len; i++)
 	{
+		//this is the easy part... the iteration of lowercase which is the first index of the array will be equal to the lowercase letter of word at the first iteration.
 		lowercase[i] = tolower(word[i]);
 	}
+	//make lowercase[len] = to null. to terminate the string.
 	lowercase[len] = '\0';
 }
